557b960efa46a5963cb7ee7a531773f1293e0cd9
Vijini Mallawaarachchi <vijini.13@cse.mrt.ac.lk>
2016-08-27 14:09:04 +0530

    Removed incomplete files
    Removed incomplete files

--- PE/29.py
+++
@@ -1,9 +0,0 @@
-nums = set()
-
-for a in range(2,101):
-    for b in range(2,101):
-        n = a**b
-        nums.add(n)
-
-print len(nums)
-        
--- PE/48.py
+++
@@ -1,9 +0,0 @@
-sum1 = 0
-
-for i in range(1,1001):
-    a = i**i
-    sum1+=a
-
-s = str(sum1)
-
-print s[-10:]
