c2a5044e762ed830cd69b84ae524e7bded4edf0b
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2023-12-17 18:52:40 +1030

    Update answer of Q3 to python3

--- project_euler/003 - Largest prime factor/3.py
+++ project_euler/003 - Largest prime factor/3.py
@@ -1,14 +1,15 @@
 max = 0
-num = 600851475143L
-n=3L
+num_to_test = 600851475143
+num = 600851475143
+n=3
 
 while True:
-    while num%n==0:
+    while num % n == 0:
         num /= n
         max = n
-    if num==1:
+    if num == 1:
         break
-    n+=2L
+    n += 2
 
-print "The largest prime factor of the number 600851475143: ", max
+print(f"The largest prime factor of the number {num_to_test}: {max}")
     
