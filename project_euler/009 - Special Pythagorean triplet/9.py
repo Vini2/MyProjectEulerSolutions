e356dcb2a2eb3330ac9ef76d1c6fe7477552a9b5
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2023-12-17 19:25:24 +1030

    Update answer of Q9

--- project_euler/009 - Special Pythagorean triplet/9.py
+++ project_euler/009 - Special Pythagorean triplet/9.py
@@ -1,11 +1,12 @@
 
 def preu():
-    for i in range(1,1000):
-        for j in range(1,1000):
-            for k in range(1,1000):
-                a = i+j+k
-                if(i*i+j*j==k*k and a==1000):
-                    print i*j*k
-                    return 0
+    for i in range(1, 1000):
+        for j in range(1, 1000):
+            for k in range(1, 1000):
+                a = i + j + k
+                if a == 1000:
+                    if((i * i) + (j * j) == (k * k)):
+                        print(i * j * k)
+                        return 0
 
 preu()
