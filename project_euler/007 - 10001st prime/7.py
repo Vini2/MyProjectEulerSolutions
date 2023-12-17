1d75a33aa763a0d8bdc923dda90592eaffbaeb2b
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2023-12-17 19:12:34 +1030

    Update answer of Q7 to python3

--- project_euler/007 - 10001st prime/7.py
+++ project_euler/007 - 10001st prime/7.py
@@ -6,21 +6,21 @@ def is_prime(n):
     else:
         sqr = int(math.sqrt(n)) + 1
         for divisor in range(2, sqr):
-            if n%divisor == 0:
+            if n % divisor == 0:
                 return False
     return True
 
 
-count1=1
+count1 = 1
 i = 3
 
 while True:
     
     if is_prime(i):
-        count1=count1+1
+        count1 = count1 + 1
 
     if (count1 == 10001):
-        print i
+        print(f"10001st prime number: {i}")
         break
     
     i=i+2
