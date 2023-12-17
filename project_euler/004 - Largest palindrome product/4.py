d6e187402e80d05a195e054a90e66bcd73728b8e
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2023-12-17 18:55:08 +1030

    Update answer of Q4 to python3

--- project_euler/004 - Largest palindrome product/4.py
+++ project_euler/004 - Largest palindrome product/4.py
@@ -2,13 +2,13 @@ def main():
     for i in range(9, 0, -1):
         for j in range(9, -1, -1):
             for k in range(9, -1, -1):
-                a = (i*100001)+(j*10010)+(k*1100)
+                a = (i * 100001) + (j * 10010) + (k * 1100)
 
                 for m in range(999, 101, -1):
-                    if a%m==0:
-                        d = a/m
-                        if (d>=100 and d<=999):
-                            print "Largest palindrome with 3 digit multipliers is: ", a
+                    if a % m == 0:
+                        d = a / m
+                        if (d >= 100 and d <= 999):
+                            print("Largest palindrome with 3 digit multipliers is:", a)
                             return True
 
 main()
