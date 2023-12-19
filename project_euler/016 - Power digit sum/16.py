2b84e8259e60b082be4c0744ee88bd5c72bc35df
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2023-12-20 10:22:58 +1030

    Update answer of Q16 to python3

--- project_euler/016 - Power digit sum/16.py
+++ project_euler/016 - Power digit sum/16.py
@@ -1,4 +1,4 @@
-a = 2**1000
+a = 2 ** 1000
 
 s = str(a)
 
@@ -7,4 +7,4 @@ sum1 = 0
 for i in range(len(s)):
     sum1 += int(s[i])
 
-print sum1
+print(f"Sum of the digits of the number 2**1000: {sum1}")
