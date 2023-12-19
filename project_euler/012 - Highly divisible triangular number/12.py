6a42d3c066579b7b5880d12dc5d531147ef9d4a5
Vijini Mallawaarachchi <viji.mallawaarachchi@gmail.com>
2023-12-20 10:05:21 +1030

    Update answer of Q12 to python3

--- project_euler/012 - Highly divisible triangular number/12.py
+++ project_euler/012 - Highly divisible triangular number/12.py
@@ -1,29 +1,32 @@
 import math
 
 def isPrime(m):
-    if m<3:
+    if m < 3:
         return True
     else:
-        for i in range(2,int(math.sqrt(m))+1):
-            if m%i==0:
+        for i in range(2, int(math.sqrt(m)) + 1):
+            if m % i == 0:
                 return False
-
         return True
 
 def main():
-    n=12368
-    while True:
-        tn = n*(n+1)/2
-        fact = 0
-        print tn
-        if not(isPrime(tn)):
-            for j in range(1,tn+1):
-                if tn%j==0:
-                    fact+=1
+    n = 12368
 
-            if fact>500:
-                print "Numbers is", tn
-                return 0
+    found_tn = False
+
+    while not found_tn:
+        tn = int(n * (n + 1) / 2)
+        fact = 0
+
+        if not(isPrime(tn)):
+            for j in range(1, tn + 1):
+                if tn % j == 0:
+                    fact += 1
+
+            if fact > 500:
+                print("First triangle number to have over five hundred divisors:", tn)
+                found_tn = True
+                break
             
         n+=1
 
